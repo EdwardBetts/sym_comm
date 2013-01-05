189ec97a8a23ac3039e99f82b0500d74c6c98b29
thor <thor@mintj.(none)>
2013-01-05 10:53:00 +0100

    added setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "SimpleCommodities",
+    version = "0.0.1",
+    packages = find_packages(),
+    scripts = ['gltest.py'],
+
+    install_requires = ['docutils>=0.3',
+                        'pyglet>+1.1.4'],
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+        'hello': ['*.msg'],
+    }
+
+    # metadata for upload to PyPI
+    # author = "Jason Katzwinkel",
+    # author_email = "jk@winkel.com",
+    # description = "",
+    # license = "",
+    # keywords = "",
+    # url = "",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
